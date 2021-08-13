a1ffc7f5ed0d31cac5b64cc04ad1c93943cedaaa
Yonatan Korem <yonatan.korem@gmail.com>
2021-08-13 14:45:32 +0300

    four apps created. need jest

---
+++ Workspace/projects/single-component-form/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'korem-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'single-component-form';
+}
